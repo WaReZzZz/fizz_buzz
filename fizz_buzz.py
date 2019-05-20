53cc0146e778a1e02343583caf51b420cea1a139
Afriat Yaniv <yaniv.afriat@gmail.com>
2019-05-20 17:27:25 +0200

    Create fizz_buzz.py

---
+++ fizz_buzz.py
@@ -0,0 +1,12 @@
+# Écrire un programme qui affiche les nombres de 1 à 199. Mais pour les multiples de 3,
+# afficher “Fizz” au lieu du nombre et pour les multiples de 5 afficher “Buzz”.
+# Pour les nombres multiples de 3 et 5, afficher “FizzBuzz”.
+
+# Python3
+
+
+def fizzbuzz():
+
+
+if __name__ == '__main__':
+    fizzbuzz()
